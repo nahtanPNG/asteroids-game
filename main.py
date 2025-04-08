65e5a9802bb60330fbf167b4b92f472a41c34bd4
nahtanpng <nathan.ferreiira.dev@gmail.com>
2025-04-08 17:12:10 -0300

    feat<player>: add player's movement

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pygame
+
 from constants import *
+from player import Player
+
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    # Setup
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     # Infinite Loop
     while True:
@@ -15,9 +18,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
         # Update the screen
         pygame.display.flip()
+
+        dt = fps.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
